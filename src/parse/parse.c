bbc54cbdbd2862765042e760ef4a551dccc9055b
Franco42Porto <frnicola@studient.42Porto.com>
2025-10-31 16:26:27 +0000

    feat(PARSE) procura errors

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -13,6 +13,49 @@
 #include "../../include/utils.h"
 #include "../../include/parse.h"
 
+void	case_split(char ***parse, char **tokens, int aux)
+{
+	if (equal(tokens[aux], ">"))
+		(*parse) = append_matriz((*parse), "\n");
+	else if (equal(tokens[aux], "<"))
+		(*parse) = append_matriz((*parse), "\n");
+	else if (equal(tokens[aux], "<<"))
+		(*parse) = append_matriz((*parse), "\n");
+	else if (equal(tokens[aux], ">>"))
+		(*parse) = append_matriz((*parse), "\n");
+	else if (equal(tokens[aux], "|"))
+		(*parse) = append_matriz((*parse), "\n");
+}
+
+void	shear_error(char **tokens, int aux)
+{
+	int		max;
+	char	*errors[10];
+	int		err;
+	int		tem;
+
+	errors[0] = ">";
+	errors[1] = ">>";
+	errors[2] = "<<";
+	errors[3] = "<";
+	errors[4] = "|";
+	errors[5] = NULL;
+	max = len_all(tokens);
+	err = 0;
+	while (errors[err] && tokens[aux])
+	{
+		tem = -1;
+		while (equal(tokens[aux], errors[err]) && errors[++tem])
+		{
+			if (equal(tokens[aux + 1], errors[tem]))
+				error_handle(0, "syntax error near unexpected token");
+		}
+		if (equal(tokens[aux], errors[err]) && !tokens[aux + 1])
+			error_handle(0, "syntax error near unexpected token");
+		err++;
+	}
+}
+
 char	**parse(char **tokens)
 {
 	int		aux;
@@ -25,8 +68,8 @@ char	**parse(char **tokens)
 	print_matriz_vec(tokens, "tokens");
 	while (tokens && len_all(tokens) != ++aux)
 	{
-		if (equal(tokens[aux], ">"))
-			parse = append_matriz(parse, "\n");
+		shear_error(tokens, aux);
+		case_split(&parse, tokens, aux);
 		parse = append_matriz(parse, tokens[aux]);
 	}
 	parse = append_matriz(parse, "\n");
