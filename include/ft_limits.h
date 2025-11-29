35f0aa5d7f411ac899f0ac7863ddf8dac8f2776d
Rodrigo Almada <rfalmada7@gmail.com>
2025-11-29 17:18:54 +0000

    chore(include): Renomeia limits.h para ft_limits.h
    Devido à flag -I presente no makefile, o ficheiro fazia conflito com o
    <limits.h>

--- include/ft_limits.h
+++ include/ft_limits.h
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   limits.h                                           :+:      :+:    :+:   */
+/*   ft_limits.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rodperei <rodperei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 20:37:26 by rodperei          #+#    #+#             */
-/*   Updated: 2025/11/21 20:37:35 by rodperei         ###   ########.fr       */
+/*   Updated: 2025/11/29 13:09:11 by rodperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIMITS_H
 # define FT_LIMITS_H
 
-# define PATH_MAX 65536
+# define PATH_MAX 65535
+# define PIPE_MAX 512
+# define HDOC_MAX 65535
 
 #endif
