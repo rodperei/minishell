575426d8a02168b6732d82e5a4976f5c6932336c
Rodrigo Almada <rfalmada7@gmail.com>
2025-12-05 18:27:16 +0000

    test(parse): Adiciona teste a parse()

---
+++ tests/parse_test.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_test.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodperei <rodperei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 18:06:05 by rodperei          #+#    #+#             */
+/*   Updated: 2025/12/05 18:10:29 by rodperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/shell_functions.h"
+#include "../include/utils.h"
+
+int	main(int ac, char **av)
+{
+	char **tokens;
+
+	(void)ac;
+	tokens = tokenize(ft_strdup(av[1]));
+	tokens = parse(tokens);
+	free_all(tokens);
+}
